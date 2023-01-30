961397c3f058e2fa82970ca3e1ac61803aa286cd
isabelpaiva <isxpaiva@icloud.com>
2023-01-30 20:00:53 -0300

    nukenzie

--- nukenzie/src/pages/HomePage/index.jsx
+++ nukenzie/src/pages/HomePage/index.jsx
@@ -1,12 +1,12 @@
 import "./index.css";
-// import logobranca from '../../assets/logobranca.svg'
+import logo from "../../assets/logo.svg"
 import Illustration from "../../assets/Illustration.svg";
 
 const HomePage = ({ setmainPage }) => {
   return (
     <div className="container-home">
       <div className="container-second">
-        {/* <img src={logobranca} alt="logo" /> */}
+        <img className="logo" src={logo} alt="logo" />
         <h1>Centralize o controle das suas finanças</h1>
         <p>de forma rápida e segura</p>
         <button className="button-start" onClick={() => setmainPage(false)}>
